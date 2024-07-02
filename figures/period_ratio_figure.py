6f25aaad09408dd537a2342a78e26a0822ee900d
Simon Alford <simonalford42@gmail.com>
2024-07-02 15:17:56 -0400

    update examples

--- figures/period_ratio_figure.py
+++ figures/period_ratio_figure.py
@@ -30,8 +30,8 @@ python period_ratio_figure.py --Ngrid 4 --version 24880 --compute
 python period_ratio_figure.py --Ngrid 4 --version 24880 --plot
 
 # compute and plot for pysr f2 (need to have computed for BNN before running this)
-python period_ratio_figure.py --Ngrid 4 --version 24880 --pysr_path 'sr_results/33060.pkl' --compute
-python period_ratio_figure.py --Ngrid 4 --version 24880 --pysr_path 'sr_results/33060.pkl' --plot
+python period_ratio_figure.py --Ngrid 4 --version 24880 --pysr_version 33060 --compute
+python period_ratio_figure.py --Ngrid 4 --version 24880 --pysr_version 33060 --plot
 
 # compute using 4 parallel jobs
 python period_ratio_figure.py --Ngrid 400 --version 24880 --compute --parallel_ix 0 --parallel_total 4
@@ -40,10 +40,10 @@ python period_ratio_figure.py --Ngrid 400 --version 24880 --compute --parallel_i
 python period_ratio_figure.py --Ngrid 400 --version 24880 --compute --parallel_ix 3 --parallel_total 4
 
 # collate the parallel results and save
-python period_ratio_figure.py --Ngrid 400 --version 24880 --collate --parallel_total 4
+python period_ratio_figure.py --Ngrid 400 --version 24880 --collate
 
 # copy needed files to local so we can plot
-scopy bnn_chaos_model/figures/period_results/v=43139_ngrid=6_pysr_f2/ ~/code/bnn_chaos_model/figures/period_results/
+scopy bnn_chaos_model/figures/period_results/v=43139_ngrid=6_pysr_f2_v=33060/ ~/code/bnn_chaos_model/figures/period_results/
 scopy bnn_chaos_model/figures/period_results/v=43139_ngrid=6.pkl ~/code/bnn_chaos_model/figures/period_results/
 '''
 
