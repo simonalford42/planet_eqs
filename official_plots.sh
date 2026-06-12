01c08101f94dfe121b6f077fa47c0fe47e4afc06
Simon Alford <simonalford42@gmail.com>
2026-06-12 10:27:51 -0400

    save

--- official_plots.sh
+++ official_plots.sh
@@ -1,12 +1,12 @@
 python interpret.py --version_json official_versions.json
-# cd figures
+cd figures
 python five_planet_plot.py --version_json ../official_versions.json
 python resonant_figure.py
 # note: requires torch/gpu to load trained models
 # python period_ratio_figure.py --official --version_json ../official_versions.json
 
 cd ..
-# this will take 15-30 minutes to run the first time as the predictions are computed.
+# this will take 15-45 minutes to run the first time as the predictions are computed.
 # after that, the predictions are cached
 python evaluation.py --official --version_json official_versions.json
-# python ablations.py --version_json official_versions.json
+python ablations.py --version_json official_versions.json
