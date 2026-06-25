b6320e7ac0927d87c3f7742cd6a44f40324f343d
Simon Alford <sca63@cornell.edu>
2026-06-25 09:55:42 -0400

    Update official_plots.sh

--- official_plots.sh
+++ official_plots.sh
@@ -3,7 +3,7 @@ cd figures
 python five_planet_plot.py --version_json ../official_versions.json
 python resonant_figure.py
 # note: requires torch/gpu to load trained models
-# python period_ratio_figure.py --official --version_json ../official_versions.json
+python period_ratio_figure.py --official --version_json ../official_versions.json
 
 cd ..
 # this will take 15-45 minutes to run the first time as the predictions are computed.
